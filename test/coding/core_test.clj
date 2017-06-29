aa26477cd871cb82dc92e355d90266cb744d9682
max-gui <playstation.gui@gmail.com>
2017-06-29 11:20:58 +0800

    add test suit

--- test/coding/core_test.clj
+++ test/coding/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [coding.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest fib-index-test
+  (testing "the first term to contain three digits should be 12."
+    (is (= (fib-index 3)
+           12))))
+
+(deftest sum-factors-test
+  (testing "The sum of all prime numbers below 10 should be 17."
+    (is (= (sum-factors 10)
+           17))))
+  
